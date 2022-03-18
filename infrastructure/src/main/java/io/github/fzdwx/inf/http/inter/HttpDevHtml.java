cfff2fc9890144fd4c6c6f2cbbf5bf0b2ce0ccdb
fzdwx <likelovec@gmail.com>
2022-03-18 22:50:19 +0800

    refactor http dev

--- infrastructure/src/main/java/io/github/fzdwx/inf/http/inter/HttpDevHtml.java
+++ infrastructure/src/main/java/io/github/fzdwx/inf/http/inter/HttpDevHtml.java
@@ -1,9 +1,11 @@
 package io.github.fzdwx.inf.http.inter;
 
+import cn.hutool.core.io.FileUtil;
 import io.github.fzdwx.inf.Handler;
 import io.github.fzdwx.inf.http.HttpRequest;
 import io.github.fzdwx.inf.http.HttpResponse;
 import io.github.fzdwx.inf.route.Router;
+import io.github.fzdwx.lambada.Lang;
 import io.github.fzdwx.lambada.Seq;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,23 +22,41 @@ public class HttpDevHtml implements Handler {
 
     private final Router router;
     private final String name;
+    private String apiList;
+    private String fileList;
 
     public HttpDevHtml(final String name, final Router router) {
         this.router = router;
         this.name = name;
+        init(router);
     }
 
     @Override
     public void handle(final HttpRequest request, final HttpResponse resp) throws Exception {
-        final var apiList = Seq.of(router.handlers())
-                .skip(1)
-                .map(h -> "<a href='" + h.path() + "'> " + h.method().name + " --- " + h.path() + " </a><br>")
-                .collect(Collectors.joining(""));
         final var html = """
                                  <meta charset="UTF-8">
                                  <title>%s | DEV PAGE </title> 
-                                 """ + apiList;
-
+                                 """
+                         +"Api:<br>"
+                         + apiList
+                         +"File:<br>"
+                         + fileList;
         resp.html(html.formatted(name));
+    }
+
+    private void init(final Router router) {
+        this.apiList = Seq.of(router.handlers())
+                .skip(1)
+                .map(h -> "&nbsp;&nbsp;&nbsp;&nbsp;<a href='" + h.path() + "'> " + h.method().name + " --- " + h.path() + " </a><br>")
+                .collect(Collectors.joining(""));
+
+        this.fileList = Seq.of(FileUtil.listFileNames(""))
+                .map(h -> {
+                    router.GET("/" + h, (req, response) -> {
+                        response.html(FileUtil.readString(h, Lang.CHARSET));
+                    });
+                    return "&nbsp;&nbsp;&nbsp;&nbsp;<a href='/" + h + "'> " + h + " </a><br>";
+                })
+                .collect(Collectors.joining(""));
     }
 }
