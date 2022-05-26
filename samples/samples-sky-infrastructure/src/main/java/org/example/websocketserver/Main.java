b387e68e6ed436169eff9af2ecf4eb4bfc7ef237
weiZhen <likelovec@gmail.com>
2022-05-26 16:04:58 +0800

    📸 add websocket server samples

---
+++ samples/samples-sky-infrastructure/src/main/java/org/example/websocketserver/Main.java
@@ -0,0 +1,24 @@
+package org.example.websocketserver;
+
+import http.HttpServer;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">fzdwx</a>
+ * @date 2022/5/26 16:01
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        HttpServer.create().handle((req, resp) -> {
+                    req.upgradeToWebSocket(ws -> {
+
+                        ws.mountOpen(h -> {
+                            ws.send("hello world");
+                        });
+
+                    });
+                })
+                .listen(8888)
+                .dispose();
+    }
+}
