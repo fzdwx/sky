30bf29c0800ea9dbfaee069fd56ba42a2f079b0c
fzdwx <likelovec@gmail.com>
2022-06-06 08:07:42 +0800

    📝 add doc

--- sky-infrastructure/src/main/java/core/http/ext/HttpServerRequest.java
+++ sky-infrastructure/src/main/java/core/http/ext/HttpServerRequest.java
@@ -30,31 +30,77 @@ import java.util.Collection;
  */
 public interface HttpServerRequest {
 
+    /**
+     * Release resources and end the life cycle
+     */
     void destroy();
 
+    /**
+     * this request is multipart?
+     */
     boolean multipart();
 
+    /**
+     * this request is formUrlEncoder?
+     */
     boolean formUrlEncoder();
 
+    /**
+     * @see DecoderResult
+     */
     DecoderResult decoderResult();
 
+    /**
+     * remote address
+     */
     SocketAddress remoteAddress();
 
+    /**
+     * http version
+     *
+     * @return {@link HttpVersion }
+     * @see HttpVersion#HTTP_1_1
+     * @see HttpVersion#HTTP_1_0
+     */
     HttpVersion version();
 
+    /**
+     * http headers
+     *
+     * @see io.netty.handler.codec.http.HttpHeaders
+     */
     Headers headers();
 
+    /**
+     * url params
+     * <p>
+     * e.g <code>http://www.baidu.com?a=1&b=2</code> => a=1,b=2
+     */
     KvMap params();
 
+    /**
+     * ues ssl?
+     *
+     * @return boolean
+     */
     boolean ssl();
 
+    /**
+     * json serializer.
+     */
     JsonSerializer serializer();
 
+    /**
+     * get file from form.
+     */
     default HttpServerRequest readFile(Hooks<FileUpload> hooks, String key) {
         hooks.call(readFile(key));
         return this;
     }
 
+    /**
+     * get all files from form.
+     */
     default HttpServerRequest readFiles(Hooks<Collection<FileUpload>> hooks) {
         hooks.call(readFiles());
 
@@ -75,12 +121,21 @@ public interface HttpServerRequest {
         return Netty.read(body());
     }
 
+    /**
+     * get all attributes from form.
+     */
     @Nullable
     KvMap formAttributes();
 
+    /**
+     * get file from form.
+     */
     @Nullable
     FileUpload readFile(String key);
 
+    /**
+     * get all files from form.
+     */
     @Nullable
     Collection<FileUpload> readFiles();
 
@@ -115,10 +170,19 @@ public interface HttpServerRequest {
      */
     void upgradeToWebSocket(Hooks<WebSocket> h);
 
+    /**
+     * upgrade to websocket?
+     */
     boolean isWebsocket();
 
+    /**
+     * source request.
+     */
     HttpRequest nettyRequest();
 
+    /**
+     * http content type.
+     */
     String contentType();
 
     static HttpServerRequest create(final ChannelHandlerContext ctx, final boolean ssl, AggHttpServerRequest msg, final JsonSerializer serializer) {
