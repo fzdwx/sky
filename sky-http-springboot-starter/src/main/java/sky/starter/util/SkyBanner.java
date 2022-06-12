a22f430c76bbde122159fbf89d772dc36e3cff85
fzdwx <likelovec@gmail.com>
2022-06-12 18:35:25 +0800

    to dev

--- sky-http-springboot-starter/src/main/java/sky/starter/util/SkyBanner.java
+++ sky-http-springboot-starter/src/main/java/sky/starter/util/SkyBanner.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SkyBanner {
 
-    private final static String version = "0.11.3";
+    private final static String version = "0.11.4-dev";
 
     public static void print() {
         log.info(Console.banner() + Console.cyan("  [ Sky Web Server ]  ") + Console.ANSI_RESET + "                             (v{})", version);
